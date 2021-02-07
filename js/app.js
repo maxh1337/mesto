a789468f1fd0c6954a890350295aedd52e883409
Maksim Vorobev <max11052003@yandex.ru>
2021-02-07 21:02:09 +0300

    1

--- js/app.js
+++ js/app.js
@@ -1,99 +1,97 @@
-/*let popup = document.querySelector('.popup');
-let profileName = document.querySelector('.profile__name');
-let profileAbout = document.querySelector('.profile__about');
-let userEditBtn = document.querySelector('.profile__edit-btn');
-let formCloseBtn = document.querySelector('.popup__close-btn');
-let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__input_type_username');
-let aboutInput = document.querySelector('.popup__input_type_about');
+let popup = document.querySelector(".popup");
+let profileName = document.querySelector(".profile__name");
+let profileAbout = document.querySelector(".profile__about");
+let userEditBtn = document.querySelector(".profile__edit-btn");
+let formCloseBtn = document.querySelector(".popup__close-btn");
+let formElement = document.querySelector(".popup__form");
+let nameInput = document.querySelector(".popup__input_type_username");
+let aboutInput = document.querySelector(".popup__input_type_about");
 
 function showPopup() {
-	popup.classList.add('popup_opened');
-	nameInput.value = profileName.textContent;
-	aboutInput.value = profileAbout.textContent;
+  popup.classList.add("popup_opened");
+  nameInput.value = profileName.textContent;
+  aboutInput.value = profileAbout.textContent;
 }
 
 function closePopup() {
-	popup.classList.remove('popup_opened');
+  popup.classList.remove("popup_opened");
 }
 
 function formSubmitHandler(event) {
-	event.preventDefault();
-	profileName.textContent = nameInput.value;
-	profileAbout.textContent = aboutInput.value;
-	closePopup();
+  event.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileAbout.textContent = aboutInput.value;
+  closePopup();
 }
 
-userEditBtn.addEventListener('click', showPopup);
+userEditBtn.addEventListener("click", showPopup);
 
-formCloseBtn.addEventListener('click', closePopup);
+formCloseBtn.addEventListener("click", closePopup);
 
-formElement.addEventListener('submit', formSubmitHandler);*/
+formElement.addEventListener("submit", formSubmitHandler);
 
 // Подгрузка блоков card с помощью template тега
-const SRC_LIST = [
-  { src: "./images/baikal.jpg" },
-  { src: "./images/ermitazh.jpg" },
-  { src: "./images/kizhi.jpg" },
-  { src: "./images/rodinamother.jpg" },
-  { src: "./images/card-ozero_dzeka_londona.jpg" },
-  { src: "./images/georgiy.jpg" },
+const initialCards = [
+  {
+    name: "Байкал",
+    link: "./images/baikal.jpg",
+  },
+  {
+    name: "Эрмитаж",
+    link: "./images/ermitazh.jpg",
+  },
+  {
+    name: "Кижи",
+    link: "./images/kizhi.jpg",
+  },
+  {
+    name: "Родина-мать",
+    link: "./images/rodinamother.jpg",
+  },
+  {
+    name: "Озеро Джека Лондона",
+    link: "./images/card-ozero_dzeka_londona.jpg",
+  },
+  {
+    name: "Церковь Георгия Победоносца",
+    link: "./images/georgiy.jpg",
+  },
 ];
-const TITLE_LIST = [
-  { title: "Байкал" },
-  { title: "Эрмитаж" },
-  { title: "Кижи" },
-  { title: "Родина-мать" },
-  { title: "Озеро Джека Лондона" },
-  { title: "Церковь Георгия Победоносца" },
-];
-
-/*const cardContainer = document.querySelector(".cards");
-const templateEl = document.querySelector(".template");
-
-function render() {
-  const html = TITLE_LIST.map(getItem);
-
-  cardContainer.append(...html);
-}
-render();
-
-function getItemHTML(item) {
-  return `<article class="card">
-			<img src="${item.src}" alt="Озеро Байкал" class="card__image">
-				<div class="card__action">
-					<h2 class="card__title">${item.title}</h2>
-					<button type="button" class="card__like elem-click"></button>
-				</div>
-			</article>`;
-}
-function getItem(item) {
-  const newItem = templateEl.content.cloneNode(true);
-  const TitleEl = newItem.querySelector(".card__title");
-  TitleEl.textContent = item.title;
-
-  return newItem;
-}
-render();*/
 const cardContainer = document.querySelector(".cards");
 const templateEl = document.querySelector(".template");
 
 function render() {
-  const html = TITLE_LIST.map(getItem);
+  const html = initialCards.map(getItem);
   cardContainer.append(...html);
+  cardContainer.append(...src);
 }
-
 function getItem(item) {
   const newItem = templateEl.content.cloneNode(true);
-  const TitleEl = newItem.querySelector(".card__title");
-  TitleEl.textContent = item.title;
+  const nameEl = newItem.querySelector(".card__title");
+  nameEl.textContent = item.name;
+
+  const linkEl = newItem.querySelector(".card__image");
+  linkEl.src = item.link;
+
+  const removeCard = newItem.querySelector(".card__remove");
+  removeCard.addEventListener("click", handleRemove);
+
+  const filledLike = newItem.querySelector(".card__like");
+  filledLike.addEventListener("click", handleLike);
+
   return newItem;
 }
-render();
-/*function getItem(item) {
-    const newItem = templateEl.content.cloneNode(true);
-    const headerEl = newItem.querySelector('.card__title');
-    headerEl.textContent = item.title;
 
-    return newItem;
-}*/
+function handleRemove(event) {
+  const targetEl = event.target;
+  const targetItem = targetEl.closest(".card");
+  targetItem.remove();
+}
+
+function handleLike(event) {
+  const targetEl = event.target;
+  const targetItem = targetEl.closest(".card__like");
+  targetItem.classList.toggle("card__like_filled");
+}
+
+render();
