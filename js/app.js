5307e5c218392cb3017df06a8f051e53e9d5e9a2
Maksim Vorobev <max11052003@yandex.ru>
2021-02-12 18:35:20 +0300

    final reworks

--- js/app.js
+++ js/app.js
@@ -1,76 +1,86 @@
 //Будет много комментариев, я тупой. И они помогут мне потом понять, что я вообще тут накодил) :) Thanks.
-
-
-let popup = document.querySelector(".popup");
-let profileName = document.querySelector(".profile__name");
-let profileAbout = document.querySelector(".profile__about");
-let userEditBtn = document.querySelector(".profile__edit-btn");
-let formCloseBtn = document.querySelector(".popup__close-btn");
-let formElement = document.querySelector(".popup__form");
-let nameInput = document.querySelector(".popup__input_type_username");
-let aboutInput = document.querySelector(".popup__input_type_about");
+const profilePopup = document.querySelector(".popup_profile");
+const profileName = document.querySelector(".profile__name");
+const profileAbout = document.querySelector(".profile__about");
+const userEditBtn = document.querySelector(".profile__edit-btn");
+const formCloseBtn = profilePopup.querySelector(".popup__close-btn");
+const formElement = profilePopup.querySelector(".popup__form");
+const nameInput = document.querySelector(".popup__input_type_username");
+const aboutInput = document.querySelector(".popup__input_type_about");
 
 //первый popup с редактированием информации о человеке
-function showPopup() {  //открыть
+function showPopup(popup) {
   popup.classList.add("popup_opened");
-  nameInput.value = profileName.textContent;
-  aboutInput.value = profileAbout.textContent;
 }
-function closePopup() { // закрыть
+
+function closePopup(popup) {
   popup.classList.remove("popup_opened");
 }
 
-userEditBtn.addEventListener("click", showPopup);
+function showProfilePopup() {
+  showPopup(profilePopup)
+  nameInput.value = profileName.textContent;
+  aboutInput.value = profileAbout.textContent;
+}
+function closeProfilePopup() {
+   closePopup(profilePopup);
+}
 
-formCloseBtn.addEventListener("click", closePopup);
+userEditBtn.addEventListener("click", showProfilePopup);
+
+formCloseBtn.addEventListener("click", closeProfilePopup);
 
 formElement.addEventListener("submit", formSubmitHandler);
 
-function formSubmitHandler(event) { //после изменения данных, закрыть popup
+function formSubmitHandler(event) {
+  //после изменения данных, закрыть popup
   event.preventDefault();
   profileName.textContent = nameInput.value;
   profileAbout.textContent = aboutInput.value;
-  closePopup();
+  closeProfilePopup();
 }
 
 // Popup с додавлением новой карточки (place)
 
-let place = document.querySelector(".popup_place");
-let placeCloseBtn = document.querySelector(".popup__close-btn_place");
-// let placeForm = document.querySelector(".place__form");
-let placeName = document.querySelector(".popup__input_type_name");
-let placeLink = document.querySelector(".popup__input_type_link");
-let placeOpenBtn = document.querySelector(".profile__add-btn");
-let placeSaveBtn = document.querySelector(".popup__save-btn");
-
+const place = document.querySelector(".popup_place");
+const placeCloseBtn = document.querySelector(".popup__close-btn_place");
+const placeName = document.querySelector(".popup__input_type_name");
+const placeLink = document.querySelector(".popup__input_type_link");
+const placeOpenBtn = document.querySelector(".profile__add-btn");
+const placeSaveBtn = document.querySelector(".popup__save-btn");
+const popupGalleryTitle = document.querySelector(".popup__title_gallery");
+const popupGalleryImage = document.querySelector(".popup__image");
+const galleryCloseBtn = document.querySelector(".popup__close-btn_gallery");
 //открыть popup
-function showPlace() {
-  place.classList.add("popup_opened");
+function showPlacePopup() {
+  showPopup(place)
 }
 //закрыть popup
-function closePlace() {
-  place.classList.remove("popup_opened"); 
+function closePlacePopup() {
+  closePopup(place)
 }
-placeOpenBtn.addEventListener("click", showPlace);
+placeOpenBtn.addEventListener("click", showPlacePopup);
 
-placeCloseBtn.addEventListener("click", closePlace);
+placeCloseBtn.addEventListener("click", closePlacePopup);
 
 placeSaveBtn.addEventListener("submit", saveNewPlace);
 
 //функия добавления новой карточки
 
 function saveNewPlace(evt) {
-evt.preventDefault();
-const inputName = placeName.value; //приравниваю значения
-const inputLink = placeLink.value;//приравниваю значения
-const savePlace = getItem({link: inputLink, name: inputName});  //добавляю в функцию getItem введенные значения Имени и ссылки на карточку
-cardContainer.prepend(savePlace); //вставляю в блок с карточками собранную конструкцию в самое начало (prepend)
-inputName.value = "" ; // значения будут вставленны в "" скобках
-inputLink.value = "" ; 
-closePlace(evt); // по окончанию работы функции закрыть popup(блока place)
+  evt.preventDefault();
+  const inputName = placeName.value; //приравниваю значения
+  const inputLink = placeLink.value; //приравниваю значения
+  const savePlace = getItem({ link: inputLink, name: inputName }); //добавляю в функцию getItem введенные значения Имени и ссылки на карточку
+  cardContainer.prepend(savePlace); //вставляю в блок с карточками собранную конструкцию в самое начало (prepend)
+  inputName.value = ""; // значения будут вставленны в "" скобках
+  inputLink.value = "";
+  closePlace(evt); // по окончанию работы функции закрыть popup(блока place)
 }
 
-let formListener = document.querySelector(".popup__form_place").addEventListener("submit", saveNewPlace);
+const formListener = document
+  .querySelector(".popup__form_place")
+  .addEventListener("submit", saveNewPlace);
 
 // Подгрузка блоков card с помощью template тега
 const initialCards = [
@@ -107,27 +117,27 @@ function render() {
   cardContainer.append(...html);
 }
 function getItem(item) {
-  const newItem = templateEl.content.cloneNode(true);  //клонируем template конструкцию
-  const nameEl = newItem.querySelector(".card__title"); 
-  nameEl.textContent = item.name;  
+  const newItem = templateEl.content.cloneNode(true); //клонируем template конструкцию
+  const nameEl = newItem.querySelector(".card__title");
+  nameEl.textContent = item.name;
 
-  const linkEl = newItem.querySelector(".card__image");
-  linkEl.src = item.link;
+  const cardImage = newItem.querySelector(".card__image");
+  cardImage.src = item.link;
+  cardImage.alt = item.name;
 
   const removeCard = newItem.querySelector(".card__remove");
-  removeCard.addEventListener("click", handleRemove); //удаление карточки 
+  removeCard.addEventListener("click", handleRemove); //удаление карточки
 
   const filledLike = newItem.querySelector(".card__like");
   filledLike.addEventListener("click", handleLike); //лайк на карточку
-  
+
   // открытие popup фоточек карточек
 
   
-  const galleryCloseBtn = document.querySelector(".popup__gallery-btn");
-  const cardImage = newItem.querySelector(".card__image");
   const galleryOpenBtn = cardImage;
-  galleryOpenBtn.addEventListener("click", showGallery);
-  galleryCloseBtn.addEventListener("click", closeGallery);
+  galleryOpenBtn.addEventListener("click", () => {
+    showGallery(item);
+  });
   return newItem;
 }
 
@@ -140,33 +150,24 @@ function handleRemove(event) {
 
 //как и в примере с удалением, лайк ставится на самый близкий
 function handleLike(event) {
-  const targetEl = event.target;   
-  const targetItem = targetEl.closest(".card__like"); 
+  const targetEl = event.target;
+  const targetItem = targetEl.closest(".card__like");
   targetItem.classList.toggle("card__like_filled");
 }
-
-
 
 // Открытие popup c карточкой
 
 const galleryPopup = document.querySelector(".popup_gallery_place");
 
-function showGallery(evt) {
-// console.dir(evt.target)
-galleryPopup.classList.add("popup_opened");
-const targetEl = evt.target;
-const targetItem = targetEl.closest('.card');
-const cardTitle = targetItem.querySelector('.card__title');
-const popupGalleryTitle = document.querySelector(".popup__title_gallery");
-const popupGalleryImage = document.querySelector(".popup__image");
-const galleryImage = targetItem.querySelector("card__image");
-popupGalleryTitle.textContent = cardTitle.textContent;
-// popupGalleryImage.src = evt.galleryImage.src;
-popupGalleryImage.src = evt.target.src
+galleryCloseBtn.addEventListener("click", closeGallery);
+function showGallery(item) {
+  showPopup(galleryPopup)
+  popupGalleryTitle.textContent = item.name
+  popupGalleryImage.src = item.link
 }
 
 function closeGallery() {
-	galleryPopup.classList.remove('popup_opened');
+  closePopup(galleryPopup)
 }
 
 render();
